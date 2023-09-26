910ab206f69d6a6ab3c34320fca0c9c8880014ee
PkPawarat <Popawarat@gmail.com>
2023-09-26 21:23:12 +1000

    combine Lauren and adjust variable Unstable

--- TurtlebotFollowStraightLine.m
+++ TurtlebotFollowStraightLine.m
@@ -18,7 +18,12 @@ classdef TurtlebotFollowStraightLine < handle
 
     properties
     QRCodeFound = false; % On startup, this will be false. 
-    
+    pub_vel;% = rospublisher('/cmd_vel','DataFormat','geometry_msgs/Twist');
+    odom_;% = rossubscriber('/odom','DataFormat','struct');
+    lidar_;% = rossubscriber('/scan','DataFormat','struct');
+    camera_rgb_;% = rossubscriber('/camera/rgb/image_raw');
+    turn_duration = 5; % Turning duration in seconds
+
     ImageSub;
     VideoPlay;
     velPub;
@@ -29,140 +34,170 @@ classdef TurtlebotFollowStraightLine < handle
 
     end
 
+    methods
+        function self = TurtlebotFollowStraightLine()
+            
+            %Clear all, close all etc
+            close all;
+            clc;
 
-    function main(self)
+
+            %% SETUP
+            % Connect to ROS master
+            rosshutdown;
+
+            % rosinit(turtlebotIp);
+            rosinit;
+
+            self.pub_vel = rospublisher('/cmd_vel','geometry_msgs/Twist');
+            self.odom_ = rossubscriber('/odom','DataFormat','struct');
+            self.lidar_ = rossubscriber('/scan','DataFormat','struct');
+            self.camera_rgb_ = rossubscriber('/camera/rgb/image_raw');
+    
+            % Create ROS subscribers and publishers
+            self.ImageSub = rossubscriber('/camera/rgb/image_raw');
+            receive(self.ImageSub,10); % Wait to receive first message
+            [self.velPub,self.velMsg] = rospublisher('/cmd_vel');
+
+
         
-        %Clear all, close all etc
+            %Initialise ROS
+            figure
+            lidar_scan_data = receive(self.lidar_, 1);
+            rosPlot(lidar_scan_data,"MaximumRange",10);
+        
+            disp("1. Searching for QR Code");
+            %While QR not found, 
+            while (~(self.QRCodeFound))
+                self.ScanForQR(self);
+                pause(5);
+            end
+            disp("1. QR Code found.");
+        
     
     
-        %Initialise ROS
+            disp("3. Calculating normal line from QR code");
+            CalculateNormal(self);
     
-        display("1. Searching for QR Code");
-        %While QR not found, 
-        while (not(QRCodeFound))
-            self.ScanForQR(self);
-            pause(5);
+    
+            disp("4. Turning robot towards normal line");
+    
+    
+    
+            disp("4. Driving robot towards normal line");
+    
+    
+            disp("5. Rotating robot towards QR");
+    
+            %Do a cross check here to make sure we are in line
+            disp("6. Cross check position");
+    
+    
+    
         end
-        disp("1. QR Code found.");
-    
-
-
-        disp("3. Calculating normal line from QR code");
-        CalculateNormal(self);
-
-
-        disp("4. Turning robot towards normal line");
-
-
-
-        disp("4. Driving robot towards normal line");
-
-
-        disp("5. Rotating robot towards QR");
-
-        %Do a cross check here to make sure we are in line
-        disp("6. Cross check position");
-
-
-
     end
 
+    methods(Static)
+
+         % If the QR code can be found, then change 'QRCodeFound' to true
+        function ScanForQR(self)
+            % TODO: Store QR location
+            % ImageSub = imread("Test3.png");
+            % [msg, detectedFormat, loc] = readBarcode(img);
+            % % disp(msg);
+            % % disp(detectedFormat);
+            % % disp(loc);
+            % 
+            % currentQRCodeTarget = self.QRCodeOrder(self.GetOrder);
+            % if currentQRCodeTarget ~= msg
+            %     self.Rotate45CC(self)
+            %     return
+            % 
+            % else
+            %     %move to target by checking straighLine
+            % end
+
+            % for i = 0:45:360
+                rgb_image = receive(self.camera_rgb_,10);
+                [msg, detectedFormat, loc] = readBarcode(rgb_image);
+                % grey_image = rgb2gray(rgb_image);
+                % Logic for identifying QR code
+                currentQRCodeTarget = self.QRCodeOrder(self.GetOrder);
+                if currentQRCodeTarget == msg
+                    self.QRCodeFound = true;
+                    return;
+                end
+                %If QR not found
+                self.Rotate45CC(self);
+                pause(5);
+            % end     
+            
     
-    function SetupRos(self)
-        %% SETUP
-        % Connect to ROS master
-        rosshutdown;
-        % turtlebotIp = '172.19.115.28';
-        % rosinit(turtlebotIp);
-
-        % Create ROS subscribers and publishers
-        self.ImageSub = rossubscriber('/camera/rgb/image_raw');
-        receive(self.ImageSub,10); % Wait to receive first message
-        [self.velPub,self.velMsg] = rospublisher('/cmd_vel');
-
-        % Create video player for visualization
-        self.VideoPlay = vision.DeployableVideoPlayer;
-
-        % Load control parameters
-        % params = turtlebotcontrol;
-    end
-
-
-
-
-     % If the QR code can be found, then change 'QRCodeFound' to true
-    function ScanForQR(self)
-        % TODO: Store QR location
-        clc;
-        ImageSub = imread("Test3.png");
-        [msg, detectedFormat, loc] = readBarcode(img);
-        disp(msg);
-        disp(detectedFormat);
-        disp(loc);
-
-        currentQRCodeTarget = self.QRCodeOrder(self.GetOrder);
-        if currentQRCodeTarget ~= msg
-            self.Rotate45CC(self)
-            return
-
-        else
-            %move to target by checking straighLine
+            % If QR not found, you can add your handling logic here
+            % For example, call the Rotate45CC function
+            % Rotate45CC(self);
         end
         
+        
+        
+        %Rotate the robot 45 degrees counter-clockwise. 
+        function Rotate45CC(self)
+            disp("2. QR Code not found, turning X amount and trying again");
+               
+            % %CK NOTES
+            %     % May need to take readings of the odom to determine the
+            %     % current pose of the turtlebot and do a while loop to check
+            %     % that we are stopping the rotation at the correct time... 
+            % 
+            % % Initialize ROS node in MATLAB
+            % rosinit;
+            % 
+            % % Create a publisher for sending velocity commands
+            % cmd_vel_pub = rospublisher('/cmd_vel', 'geometry_msgs/Twist');
+            % 
+            % % Create a Twist message for the desired velocity command
+            % cmd_msg = rosmessage(cmd_vel_pub);
+            % 
+            % % Set the angular velocity to make the TurtleBot rotate
+            % cmd_msg.Angular.Z = 0.5; % Adjust the value as needed for 45-degree rotation
+            % 
+            % % Publish the command
+            % send(cmd_vel_pub, cmd_msg);
+            % 
+            % % Sleep for a duration to rotate the TurtleBot
+            % pause(10); % Adjust the duration as needed for 45-degree rotation
+            % 
+            % % Stop the TurtleBot by sending a zero velocity command
+            % cmd_msg.Angular.Z = 0;
+            % send(cmd_vel_pub, cmd_msg);
+            % 
+            % % Shutdown the ROS node when done
+            % rosshutdown;
+            for i = 1:turning_duration-1
+                self.sendBotVel(0, 0, 0.157, 0);
+            end    
+            self.sendBotVel(0, 0, 0, 0);
+        end
+    
+    
+        %Store all necessary values as properties within the class rather than
+        %trying to pass them in and out of functions
+        function CalculateNormal(self)
+            disp("QR Code not found, turning X amount and trying again");
+            rgb_image = receive(camera_rgb_,10);
+            grey_image = rgb2gray(rgb_image);
+            % find the normal logic   
+        end
 
-        % If QR not found, you can add your handling logic here
-        % For example, call the Rotate45CC function
-        % Rotate45CC(self);
+        function sendBotVel(linear_x, linear_y, angular_x, angular_y)
+            bot_vel = rosmessage('geometry_msgs/Twist');
+            bot_vel.twist.twist.linear.x = linear_x;
+            bot_vel.twist.twist.linear.y = linear_y;
+            bot_vel.twist.twist.angular.x = angular_x;
+            bot_vel.twist.twist.angular.y = angular_y;
+            send(pub_vel,bot_vel);
+        end
+
     end
-    
-    
-    
-    %Rotate the robot 45 degrees counter-clockwise. 
-    function Rotate45CC(self)
-        disp("2. QR Code not found, turning X amount and trying again");
-           
-        %CK NOTES
-            % May need to take readings of the odom to determine the
-            % current pose of the turtlebot and do a while loop to check
-            % that we are stopping the rotation at the correct time... 
-
-        % Initialize ROS node in MATLAB
-        rosinit;
-
-        % Create a publisher for sending velocity commands
-        cmd_vel_pub = rospublisher('/cmd_vel', 'geometry_msgs/Twist');
-    
-        % Create a Twist message for the desired velocity command
-        cmd_msg = rosmessage(cmd_vel_pub);
-    
-        % Set the angular velocity to make the TurtleBot rotate
-        cmd_msg.Angular.Z = 0.5; % Adjust the value as needed for 45-degree rotation
-    
-        % Publish the command
-        send(cmd_vel_pub, cmd_msg);
-    
-        % Sleep for a duration to rotate the TurtleBot
-        pause(10); % Adjust the duration as needed for 45-degree rotation
-    
-        % Stop the TurtleBot by sending a zero velocity command
-        cmd_msg.Angular.Z = 0;
-        send(cmd_vel_pub, cmd_msg);
-    
-        % Shutdown the ROS node when done
-        rosshutdown;
-    end
-
-
-    %Store all necessary values as properties within the class rather than
-    %trying to pass them in and out of functions
-    function CalculateNormal(self)
-     disp("QR Code not found, turning X amount and trying again");
-    
-    
-    
-    end
-
-
-
 
 end
