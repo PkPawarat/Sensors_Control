695b472295d650a6089e785a7a359d9e74661d3b
ConnorKeogh <connor.keogh@student.uts.edu.au>
2023-10-04 10:07:32 +0200

    Adding Rotation algorithm as separate file while in testing.

---
+++ testRotateRobot.m
@@ -0,0 +1,10 @@
+disp("Test robot angle starting");
+RotateRobot(90); 
+
+disp("Test robot angle complete");
+pause(5); 
+disp("Test robot reverse angle starting");
+
+RotateRobot(-90);
+
+disp("Test robot reverse angle complete");
